fb151c5352796cefd88a3234814ac2ddce0372c3
Sebastien Martin <sebastienmartin@linkernetworks.com>
2021-02-18 18:34:33 +0800

    test

--- .github/actions/cat-facts/entrypoint.sh
+++ .github/actions/cat-facts/entrypoint.sh
@@ -3,5 +3,5 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
-sh -c "echo $GITTOKEN"
-sh -c "echo $INPUT_FIRSTGREETING"
+sh -c "echo Token is $GITTOKEN"
+sh -c "echo Greeeting is $INPUT_FIRSTGREETING"
